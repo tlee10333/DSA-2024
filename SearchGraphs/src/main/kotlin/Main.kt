b4c9abd0d5c9bff559f875d628efa800a41792f9
tlee10333 <trinityl10333@gmail.com>
2024-02-25 18:26:10 -0500

    Adding Graph Searching

---
+++ SearchGraphs/src/main/kotlin/Main.kt
@@ -0,0 +1,95 @@
+package org.example
+
+class Graph<VertexType> {
+    private var vertices: MutableSet<VertexType> = mutableSetOf()
+    private var edges: MutableMap<VertexType, MutableSet<VertexType>> = mutableMapOf()
+
+    /**
+     * Add the vertex [v] to the graph
+     * @param v the vertex to add
+     * @return true if the vertex is successfully added, false if the vertex
+     *   was already in the graph
+     */
+
+
+    //Setter & Getter Methods
+    fun getVertices(): MutableSet<VertexType>{
+        return vertices
+    }
+
+    fun getEdges(): MutableMap<VertexType, MutableSet<VertexType>>{
+        return edges
+    }
+
+    fun addVertex(v: VertexType): Boolean {
+        if (vertices.contains(v)) {
+            return false
+        }
+        vertices.add(v)
+        return true
+    }
+
+    /**
+     * Add an edge between vertex [from] connecting to vertex [to]
+     * @param from the vertex for the edge to originate from
+     * @param to the vertex to connect the edge to
+     * @return true if the edge is successfully added and false if the edge
+     *     can't be added or already exists
+     */
+    fun addEdge(from: VertexType, to: VertexType): Boolean {
+        if (!vertices.contains(from) || !vertices.contains(to)) {
+            return false
+        }
+        edges[from]?.also { currentAdjacent ->
+            if (currentAdjacent.contains(to)) {
+                return false
+            }
+            currentAdjacent.add(to)
+        } ?: run {
+            edges[from] = mutableSetOf(to)
+        }
+        return true
+    }
+
+    /**
+     * Clear all vertices and edges
+     */
+    fun clear() {
+        vertices = mutableSetOf()
+        edges = mutableMapOf()
+    }
+
+}
+//***********************************************************************************************************************************************
+
+
+
+fun dijksta(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name = "Kotlin"
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println("Hello, " + name + "!")
+
+    for (i in 1..5) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        println("i = $i")
+    }
+}
